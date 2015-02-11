ba1300814841fba51cf5605c8fcf9d2ef7a2ea62
Eric Yanush <ericyanush@gmail.com>
2015-02-11 02:07:10 -0600

    Added forward declarations for all Output Functions

--- src/Outputs.h
+++ src/Outputs.h
@@ -6,25 +6,54 @@
 #include <Arduino.h>
 #include <pin.h>
 
+
+#ifdef PWM_BY_TIMER
+void pwmInit( void );
+#endif //PWM_BY_TIMER
+
+
+void pinInit();
+
+void pidInit();
+
+void resetOutputs();
+
+void resetHeatOutput(byte vessel);
+
+#if defined PID_FLOW_CONTROL && defined PID_CONTROL_MANUAL
+void processPID_FLOW_CONTROL(byte vessel);
+#endif // defined PID_FLOW_CONTROL && defined PID_CONTROL_MANUAL
+
+void processHeatOutputsPIDEnabled(const byte vessel[]);
+
+void processHeatOutputsNonPIDEnabledWithHeatOn(const byte vessel[]);
+
+void processHeatOutputsNonPIDEnabledWithHeatOff(const byte vessel[]);
+void processHeatOutputsNonPIDEnabled(const byte vessel[]);
+
+void processHeatOutputs();
+
+#ifdef PVOUT
+void updateValves();
+
+void setValves(unsigned long vlvBits);
+
+void processAutoValve();
+#endif //#ifdef PVOUT
+
+unsigned long computeValveBits();
+
+boolean vlvConfigIsActive(byte profile);
+
+void boilController ();
+
+//Map AutoValve Profiles to Vessels
 byte vesselAV(byte vessel);
 byte vesselVLVHeat(byte vessel);
 byte vesselVLVIdle(byte vessel);
-
-void resetOutputs();
-void resetHeatOutput(byte vessel);
-
-boolean vlvConfigIsActive(byte profile);
-void processHeatOutputs();
-void pinInit();
-void pidInit();
-
-void boilController ();
-void setValves(unsigned long vlvBits);
-void updateValves();
-void resetHeatOutput(byte vessel);
-unsigned long computeValveBits();
-void processAutoValve();
-
 pin * vesselMinTrigger(byte vessel);
+byte autoValveBitmask(void);
+byte getHeatPower (byte vessel);
+
 
 #endif
