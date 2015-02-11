1c5681b20c224fb7127f967457c0e09032904b8f
Eric Yanush <ericyanush@gmail.com>
2015-02-11 02:12:07 -0600

    Add forward declarations for all StepLogic Functions

--- src/StepLogic.h
+++ src/StepLogic.h
@@ -3,49 +3,98 @@
 
 #warning Fix me! Replace Forward Declarations
 
-void programThreadsUpdate();
 void programThreadsInit();
-struct ProgramThread *programThreadInit(byte recipe);
-struct ProgramThread *programThreadInit(byte recipe, byte brewStep);
-byte getFirstStepTemp(byte recipe);
+void programThreadsUpdate();
 
-unsigned long calcGrainVolume(byte recipe);
-unsigned long calcGrainLoss(byte recipe);
-
-unsigned long calcRefillVolume(byte recipe);
-unsigned long calcPreboilVol(byte recipe);
-
-void brewStepFill(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepDelay(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepPreheat(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepGrainIn(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepRefill(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepDoughIn(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepAcid(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepProtein(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepSacch(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepSacch2(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepMashOut(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepMashHold(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepSparge(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepBoil(enum StepSignal signal, struct ProgramThread *thread);
-void brewStepChill(enum StepSignal signal, struct ProgramThread *thread);
-
-
-unsigned long calcStrikeVol(byte recipe);
-byte calcStrikeTemp(byte recipe);
-unsigned long calcSpargeVol(byte recipe);
-
+/**
+* Used to determine if the given step is the active step in the program.
+*/
 boolean brewStepIsActive(byte brewStep);
+
+/**
+* Usd to determine if the given ZONE is the active ZONE in the program.
+* Returns true is any step in the given ZONE is the active step, false otherwise.
+*/
 boolean zoneIsActive(byte brewZone);
+
+byte programThreadActiveStep(byte threadIndex);
+
+void programThreadRecipeName(byte threadIndex, char *returnValue);
+
+byte programThreadRecipeIndex(byte threadIndex);
+
+struct ProgramThread *programThreadAcquire();
+
+void (*brewStepFunc(byte brewStep))(enum StepSignal, struct ProgramThread *);
+
+struct ProgramThread *programThreadInit(byte recipe);
+
+struct ProgramThread *programThreadInit(byte recipe, byte brewStep);
+
+void programThreadSave(struct ProgramThread *thread);
+
+void programThreadSetStep(struct ProgramThread *thread, byte brewStep);
 
 boolean brewStepZoneInUse(byte brewStep);
 
-void brewStepSignal(byte brewStep, enum StepSignal signal);
-void programThreadResetAll();
 void programThreadSignal(struct ProgramThread *thread, enum StepSignal signal);
-void programThreadSave(struct ProgramThread *thread);
+
+//Supports BTnic commands against brewsteps with no knowledge of threads
+void brewStepSignal(byte brewStep, enum StepSignal signal);
+
+void programThreadResetAll();
+
+void brewStepFill(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepDelay(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepPreheat(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepGrainIn(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepRefill(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepMashHelper(byte mashStep, enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepDoughIn(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepAcid(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepProtein(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepSacch(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepSacch2(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepMashOut(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepMashHold(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepSparge(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepBoil(enum StepSignal signal, struct ProgramThread *thread);
+
+void brewStepChill(enum StepSignal signal, struct ProgramThread *thread);
 
 void resetSpargeValves();
+
+#ifdef SMART_HERMS_HLT
+void smartHERMSHLT();
+#endif
+
+unsigned long calcStrikeVol(byte recipe);
+unsigned long calcSpargeVol(byte recipe);
+unsigned long calcRefillVolume(byte recipe);
+unsigned long calcPreboilVol(byte recipe);
+unsigned long calcGrainLoss(byte recipe);
+unsigned long calcGrainVolume(byte recipe);
+
+/**
+* Calculates the strike temperature for the mash.
+*/
+byte calcStrikeTemp(byte recipe);
+
+byte getFirstStepTemp(byte recipe);
+
 
 #endif
