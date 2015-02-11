812eac55b5496ec21edd27bd43303a45384115b4
Eric Yanush <ericyanush@gmail.com>
2015-02-11 02:08:18 -0600

    Add forward declarations for all Timer functions

--- src/Timer.h
+++ src/Timer.h
@@ -2,13 +2,22 @@
 #define TIMER_H
 
 #warning Fix me! Replace Forward Declarations
+#include <Arduino.h>
+
+void setTimer(byte timer, int minutes);
+
+void pauseTimer(byte timer);
+
+void clearTimer(byte timer);
+
+void updateTimers();
+
+//This function allows modulation of buzzer when the alarm is on.
+void updateBuzzer();
 
 void setAlarm(boolean alarmON);
-void updateTimers();
-void setTimer(byte timer, int minutes);
-void clearTimer(byte timer);
-void pauseTimer(byte timer);
-void updateBuzzer();
+
 void setBuzzer(boolean alarmON);
+
 
 #endif
