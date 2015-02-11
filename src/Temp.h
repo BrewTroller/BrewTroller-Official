30c9382be1e0ce00923eeec0fc698c4b160926f0
Eric Yanush <ericyanush@gmail.com>
2015-02-11 02:08:44 -0600

    Add forward declarations for all Temp(erasure) functions

--- src/Temp.h
+++ src/Temp.h
@@ -3,8 +3,26 @@
 
 #warning Fix me! Replace forward declarations
 
-void updateTemps();
+#include <Arduino.h>
+#include "Temp.h"
+#include "HWProfile.h"
+
 void tempInit();
+
+void updateTemps();
+
+boolean tsReady();
+
+boolean validAddr(byte* addr);
+
+//This function search for an address that is not currently assigned!
 void getDSAddr(byte addrRet[8]);
+
+//Returns Int representing hundreths of degree
+int read_temp(byte* addr);
+
+#if defined MASH_AVG
+void mashAvg();
+#endif
 
 #endif
